a48342aae67151afc5aab0fa15272eca90def79b
CherryBlossom <65400516+FlowerSmoothie@users.noreply.github.com>
2025-12-05 01:15:08 +0300

    Update Admin.cpp

--- CallLog_CourseProject/AAAAAAA/Admin.cpp
+++ CallLog_CourseProject/AAAAAAA/Admin.cpp
@@ -1,4 +1,4 @@
-﻿#include "Admin.h"
+#include "Admin.h"
 #include "Constants.h"
 #include "Decor.h"
 #include "LoginRegister.h"
@@ -67,7 +67,7 @@ void ViewUsers() {
 	vector<UserStruct> arr;
 	arr = ReadUsersFromFile();
 	ViewUsers(arr);
-	cout << endl;
+	printf_s("\n");
 	system("pause");
 	system("cls");
 }
@@ -80,14 +80,14 @@ vector<UserStruct> SearchForUser(vector<UserStruct> arr, string str, int task) {
 			switch (task) {
 			case 1:
 				ChangeColor(12);
-				cout << "Confirm deletion: press 1 to continue or any other key to cancel. ";
+				printf_s("Confirm deletion: press 1 to continue or any other key to cancel. ");
 				ChangeColor(15);
 				choice = _getch();
 				if (choice != '1')
 					break;
 				arr.erase(arr.begin() + i);
 				found = 1;
-				cout << endl;
+				printf_s("\n");
 				break;
 			case 2:
 				arr[i].access = 1;
@@ -104,7 +104,7 @@ vector<UserStruct> SearchForUser(vector<UserStruct> arr, string str, int task) {
 		}
 	}
 	if (found == 0) {
-		cout << "No user with such login!" << endl;
+		printf_s("No user with such login!\n");
 	}
 	return arr;
 }
@@ -113,12 +113,12 @@ void DeleteUser() {
 	vector<UserStruct> arr;
 	arr = ReadUsersFromFile();
 	string str;
-	cout << "Enter login of a user to delete: ";
+	printf_s("Enter login of a user to delete: ");
 	cin >> str;
 	arr = SearchForUser(arr, str, 1);
 	DeleteAllFromFile();
 	WriteUsersToFile(arr);
-	cout << endl;
+	printf_s("\n");
 	system("pause");
 	system("cls");
 }
@@ -126,12 +126,12 @@ void DeleteUser() {
 void ActivateUser() {
 	vector<UserStruct> arr = ReadUsersFromFile();
 	string str;
-	cout << "Enter login of an account to activate: ";
+	printf_s("Enter login of an account to activate: ");
 	cin >> str;
 	arr = SearchForUser(arr, str, 2);
 	DeleteAllFromFile();
 	WriteUsersToFile(arr);
-	cout << endl;
+	printf_s("\n");
 	system("pause");
 	system("cls");
 }
@@ -140,12 +140,12 @@ void BlockUnblockUser() {
 	vector<UserStruct> arr;
 	arr = ReadUsersFromFile();
 	string str;
-	cout << "Enter login of an account you would like to block/unblock: ";
+	printf_s("Enter login of an account you would like to block/unblock: ");
 	cin >> str;
 	arr = SearchForUser(arr, str, 3);
 	DeleteAllFromFile();
 	WriteUsersToFile(arr);
-	cout << endl;
+	printf_s("\n");
 	system("pause");
 	system("cls");
 }
@@ -155,13 +155,13 @@ void AdminMenu() {
 	while (1) {
 		system("cls");
 		PrintHeader("Menu", 13);
-		cout << " 1 - Operations with accounts\n 2 - Operations with calls\n 3 - Exit\n ";
+		printf_s(" 1 - Operations with accounts\n 2 - Operations with calls\n 3 - Exit\n ");
 		choice = _getch();
 		system("cls");
 		switch (choice) {
 		case '1':
 			PrintHeader("Operations with accounts", 13);
-			cout << "1 - Show all accounts\n 2 - Add new administrator\n 3 - Delete account\n 4 - Activate account\n 5 - Block/unblock account\n 6 - Go back\n 7 - Exit\n ";
+			printf_s("1 - Show all accounts\n 2 - Add new administrator\n 3 - Delete account\n 4 - Activate account\n 5 - Block/unblock account\n 6 - Go back\n 7 - Exit\n ");
 			choice = _getch();
 			switch (choice)
 			{
@@ -178,7 +178,7 @@ void AdminMenu() {
 			case '6':
 				break;
 			case '7':
-				cout << "Press 1 to exit or other key to stay: ";
+				printf_s("Press 1 to exit or other key to stay: ");
 				choice = _getch();
 				if (choice == '1')
 					return;
@@ -189,19 +189,19 @@ void AdminMenu() {
 			break;
 		case '2':
 			PrintHeader("Call operations", 13);
-			cout << "1 - Show data about calls\n 2 - Add new call data\n 3 - Edit call data\n 4 - Delete call\n 5 - Filter calls\n 6 - Sort calls\n 7 - Individual task\n 8 - Search calls\n 9 - Back\n 0 - Exit\n ";
+			printf_s("1 - Show data about calls\n 2 - Add new call data\n 3 - Edit call data\n 4 - Delete call\n 5 - Filter calls\n 6 - Sort calls\n 7 - Individual task\n 8 - Search calls\n 9 - Back\n 0 - Exit\n ");
 			choice = _getch();
 			switch (choice) {
 			case '1':
-				cout << '\n';
+				printf_s("\n");
 				ViewTalk();
 				break;
 			case '2':
 				AddTalk(); break;
 			case '3':
-				cout << '\n';
+				printf_s("\n");
 				ViewTalk();
-				cout << '\n';
+				printf_s("\n");
 				EditTalk();
 				break;
 			case '4':
@@ -217,7 +217,7 @@ void AdminMenu() {
 			case '9':
 				break;
 			case '0':
-				cout << "Press 1 to exit or other key to stay: ";
+				printf_s("Press 1 to exit or other key to stay: ");
 				choice = _getch();
 				if (choice == '1')
 					return;
@@ -226,7 +226,7 @@ void AdminMenu() {
 			}
 			break;
 		case '3':
-			cout << "Press 1 to exit or other key to stay: ";
+			printf_s("Press 1 to exit or other key to stay: ");
 			choice = _getch();
 			if (choice == '1')
 				return;
